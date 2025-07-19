c3098ea13832c24c480bac66ceb962940e354fc5
Ngocha2607 <ngocha2k.ln@gmail.com>
2025-07-19 22:41:18 +0700

    feat: initialize NestJS application with Cats module and basic CRUD functionality
    - Added package.json with dependencies and scripts for NestJS application.
    - Created AppController and AppService for handling basic requests.
    - Implemented CatsController and CatsService for managing cat entities.
    - Added DTOs for creating and updating cats.
    - Implemented unit tests for AppController, CatsController, and their services.
    - Set up Jest for testing and added e2e tests for the application.
    - Configured TypeScript with appropriate settings for the project.

---
+++ src/cats/cats.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+import { Cat } from './entities/cat.entity';
+
+@Injectable()
+export class CatsService {
+  private readonly listCats: Cat[] = [];
+  create(createCatDto: CreateCatDto) {
+    return this.listCats.push(createCatDto);
+  }
+
+  findAll(): Cat[] {
+    return this.listCats;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} cat`;
+  }
+
+  update(id: number, updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} cat`;
+  }
+}
